fb9d8959180abc7e30e130650169cc0dcb00cee4
Rua <rua.elkasheef@gmail.com>
2024-04-25 14:41:31 -0400

    adding firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import './App.css';
 import { auth, db } from './firebase/init';
-import { collection, addDoc } from "firebase/firestore"
+import { collection, addDoc, getDocs, getDoc, doc } from "firebase/firestore"
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
 
 function App() {
   const[user, setUser] = React.useState({});
   const [loading, setLoading] = React.useState(true);
+
+  function createPost() {
+    const post = {
+      title: "Land a $100k job",
+      description: "Finish Frontend Simplified",
+    };
+    addDoc(collection(db, "posts"), post);
+  }
+
+  async function getAllPosts() {
+    const data = await getDocs(collection(db, "posts"));
+    const posts = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+    console.log(posts)
+  }
+
+  async function getPostById() {
+    const hardCodedId = "OSy9hHoOO5i3cNJsD0db"
+    const docRef = doc(db, "posts", hardCodedId);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      const post = docSnap.data();
+    }
+    console.log(doc)
+  }
 
   React.useEffect(() => {
     onAuthStateChanged(auth, (user) => {
@@ -51,6 +75,9 @@ function App() {
       <button onClick={login}>Login</button>
       <button onClick={logout}>Logout</button>
       {loading? 'loading...' : user.email}
+      <button onClick={createPost} >Create Post</button>
+      <button onClick={getAllPosts} >Get All Posts</button>
+      <button onClick={getPostById} >Get Post By Id</button>
     </div>
   );
 }
